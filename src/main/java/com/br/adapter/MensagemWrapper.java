7f3c60d84f21a464d4d948f4be7297820761d0ee
Tetzdesen <Tetzdesen@gmail.com>
2025-03-17 15:22:11 -0300

    Refatoração

---
+++ src/main/java/com/br/adapter/MensagemWrapper.java
@@ -0,0 +1,18 @@
+package com.br.adapter;
+
+/**
+ *
+ * @author tetzner
+ */
+public class MensagemWrapper {
+
+    private final String mensagem;
+
+    public MensagemWrapper(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+}
